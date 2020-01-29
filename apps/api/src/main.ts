377a304d27bfc3b5883ac76c3be346edab73e024
Rafal Galka <rafal.galka@modernweb.pl>
2020-01-29 16:31:26 +0100

    feat(api): global prefix, port configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
+
 bootstrap();
