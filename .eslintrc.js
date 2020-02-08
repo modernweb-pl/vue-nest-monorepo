64c7251e9eae5f135b7407a7fa2acc532f6c2aa5
Rafal Galka <rafal.galka@modernweb.pl>
2020-02-08 23:01:36 +0100

    chore(eslint): unify prettier, console, debugger rules

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'prettier/prettier': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      env: {
+        node: true,
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
