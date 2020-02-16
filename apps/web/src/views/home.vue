6db810155f8e937435415c6fcbd53c89a7725789
Rafal Galka <rafal.galka@modernweb.pl>
2020-02-17 00:34:05 +0100

    chore(ci): e2e tests

--- apps/web/src/views/home.vue
+++ apps/web/src/views/home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home text-center">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
