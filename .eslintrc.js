12cb31cc440e59f9548d0fbe156835dcd609d5c9
Rafal Galka <rafal.galka@modernweb.pl>
2020-02-16 16:16:20 +0100

    chore(ci): yarn install --frozen-lockfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,5 @@
 const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-console.log(process.env.NODE_ENV);
-
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
