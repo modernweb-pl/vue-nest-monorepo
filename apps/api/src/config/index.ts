c334e77494ddf05229d0a88927e9fd649ab76e8d
Rafal Galka <rafal.galka@modernweb.pl>
2023-05-18 11:56:01 +0200

    chore(package): upgrade packages

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -1,5 +1,5 @@
-import Joi from '@hapi/joi';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import Joi from 'joi';
 
 export const configSchema = Joi.object({
   NODE_ENV: Joi.string()
